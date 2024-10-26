62c5d7c99ad0fcaf29fc923b3ac30783e82ad80e
Valkach <valentinminkac@gmail.com>
2024-10-26 18:53:46 +0300

    pages

--- AlgoLib/User/urls.py
+++ AlgoLib/User/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/', views.user_view, name='user_view'),  # Додаємо маршрут для user_view
+    path('user/', views.user_view, name='user_view'),
+    path('register/', views.user_register, name='user_register'),
+    path('login/', views.user_login, name='user_login'),
 ]
--- AlgoLib/User/views.py
+++ AlgoLib/User/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def user_view(request):
     return render(request, 'user/user.html')
+def user_register(request):
+    return render(request, 'user/register.html')
+def user_login(request):
+    return render(request, 'user/login.html')
