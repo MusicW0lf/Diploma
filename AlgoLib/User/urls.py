cd869b68654aa3bd37728c4741c04bdf57622c64
Valkach <valentinminkac@gmail.com>
2024-11-03 22:44:56 +0200

    url to "/". footer start. header rework start. Main page start creation. favicon image added. logo image added. last main -> main-no

--- AlgoLib/User/urls.py
+++ AlgoLib/User/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.user_main, name='user_main'),
     path('main/', views.user_main, name='user_main'),
     path('user/', views.user_user, name='user_user'),
     path('register/', views.user_register, name='user_register'),
