3898b53d09ee469a73f5b8e0503de25766394c2a
Valkach <valentinminkac@gmail.com>
2024-11-04 00:01:34 +0200

    main-no -> theme. added new pages. Added connection to pages. Now no header in login and register. New buttons in header.

--- AlgoLib/User/urls.py
+++ AlgoLib/User/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.user_main, name='user_main'),
     path('main/', views.user_main, name='user_main'),
+    path('theme/', views.user_theme, name='user_theme'),
+    path('subscription/', views.user_subscription, name='user_subscription'),
+    path('courses/', views.user_courses, name='user_courses'),
+    path('laboratory/', views.user_laboratory, name='user_laboratory'),
     path('user/', views.user_user, name='user_user'),
     path('register/', views.user_register, name='user_register'),
     path('login/', views.user_login, name='user_login'),
--- AlgoLib/User/views.py
+++ AlgoLib/User/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 def user_main(request):
     return render(request, 'user/main.html')
+def user_theme(request):
+    return render(request, 'user/theme.html')
+def user_subscription(request):
+    return render(request, 'user/subscription.html')
+def user_courses(request):
+    return render(request, 'user/courses.html')
+def user_laboratory(request):
+    return render(request, 'user/laboratory.html')
 def user_user(request):
     return render(request, 'user/user.html')
 def user_register(request):
